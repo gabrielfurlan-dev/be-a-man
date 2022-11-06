e1d5635b333996dcc1405ca7b3c9d5d49a267092
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2022-11-06 09:52:35 -0300

    improve: add theme colors file

---
+++ src/theme/index.tsx
@@ -0,0 +1,11 @@
+export const THEME = {
+    COLORS: {
+        BACKGROUND_BLACK: '#000',
+        BACKGROUND_WHITE: '#FFF',
+
+        FOREGROUND_BLACK: '#000',
+        FOREGROUND_GRAY: '#797979',
+        FOREGROUND_WHITE: '#FFF',
+        FOREGROUND_RED: '#FF0000',
+    }
+}
